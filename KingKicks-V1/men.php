18028544346f0a277e76c299931c3d470b32e5c5
FavoursGit <favourealist@gmail.com>
2025-05-05 04:33:08 +0100

    V1 Of KingKicks
    The VERY 1ST verion of KING KICKS

---
+++ KingKicks-V1/men.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>King Kicks </title>
+        <link rel="stylesheet" href="css/home.css">
+        <script src ="js/home.js">
+        </script>
+    </head>
+    <header>
+    <section class="nav">
+        <ul>
+            <li><a href="homepage.php"><img src="" alt="KingKicks" width="50" height="40"></a></li>
+            <li><a href="brands.html">Brands</a></li>
+            <li><a href="men.html">Men</a></li>
+            <li><a href="women.html">Women</a></li>
+            <li><a href="new.html">New</a></li>
+            <li><a href="Sale.html">Sale</a></li>
+            <li><input type ="text" placeholder="Search"></li>
+            <li><a href="https://www.instagram.com/"><img src="" alt ="Profile" width="50" height="40"></a></li>
+            <li><img src="images\basket.png" width="50" height="40"></li>
+        </ul>
+    </section>
+    </header>
+    <body>
+      <h2>Recently Searched Products</h2>>
+      <section class="recently">
+        <div class="product-box">
+            <img src="images/basket.png" alt="Image 1">
+            <div class="price">$50</div>
+        </div>
+        <div class="product-box">
+            <img src="images/basket.png" alt="Image 2">
+            <div class="price">$60</div>
+        </div>
+        <div class="product-box">
+            <img src="images/basket.png" alt="Image 3">
+            <div class="price">$70</div>
+        </div>
+        <div class="product-box">
+            <img src="images/basket.png" alt="Image 4">
+            <div class="price">$80</div>
+        </div>
+    </section>
+
+    <h2>Trending Right Now</h2>
+      <section class="trending">
+        <div class="product-box">
+            <img src="images/basket.png" alt="Image 1">
+            <div class="price">$50</div>
+        </div>
+        <div class="product-box">
+            <img src="images/basket.png" alt="Image 2">
+            <div class="price">$60</div>
+        </div>
+        <div class="product-box">
+            <img src="images/basket.png" alt="Image 3">
+            <div class="price">$70</div>
+        </div>
+        <div class="product-box">
+            <img src="images/basket.png" alt="Image 4">
+            <div class="price">$80</div>
+
+
+      </section>  
+    
+    </body>
+      <footer>
+
+      </footer>
+</html>
+    
+     
+    
+   
+    
